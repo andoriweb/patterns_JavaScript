38cffbff9f0f92da629916f18afbe3693b80f7a5
andoriweb <61490810+andoriweb@users.noreply.github.com>
2021-07-05 06:52:48 +0300

    GeoLocation

---
+++ geolocation/scripts.js
@@ -0,0 +1,27 @@
+function getLocation () {
+  navigator.geolocation.getCurrentPosition(showCoordinat, showError)
+}
+
+function showCoordinat (position) {
+  document.write('Широта :' + position.coords.latitude + '<br>')
+  document.write('Долгота :' + position.coords.longitude + '<br>')
+}
+
+function showError (error) {
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      alert ('Пользователь запретил считывать информацию о его местонахождении.')
+      break
+    case error.POSITION_UNAVAILABLE:
+      alert ('Браузер не может определить местонахождение.')
+      break
+    case error.TIMEOUT:
+      alert ('Браузер не успел определить местонахождение.')
+      break
+    case error.UNKNOWN_ERROR:
+      alert ('Произошла неопределённая ошибка.')
+      break
+  }
+}
+
+getLocation ()
