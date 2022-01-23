d8f497b491cc6cca4d474855882604394a5943c3
andoriweb <andoriweb@gmail.com>
2022-01-23 15:27:01 +0200

    tabs no active

--- Tabs/script.js
+++ Tabs/script.js
@@ -1,6 +1,23 @@
-let tabTitle = document.querySelectorAll('.tab-title li');
 let tabBlock = document.querySelectorAll('.tab-block');
+let tabTitle = document.querySelectorAll('.tab-title li');
 
-// console.log(tabTitle);
-// console.log(tabBlock);
+// Скрываем все таб блоки кроме Первого
+for (let i = 1; i < tabBlock.length; i++) {
+  tabBlock[i].style.display = 'none';
+}
 
+tabTitle.forEach(function(element) {
+  element.onclick = showTab;
+});
+
+function showTab() {
+  // Получаем элемент по Атрибуту
+  let dataTab = this.getAttribute('data-tab');
+
+  // Скрываем все таб блоки
+  for (let i = 0; i < tabBlock.length; i++) {
+    tabBlock[i].style.display = 'none';
+  }
+  // Находим элемент с нужным атрибутом
+  document.querySelector(`.tab-block[data-tab="${dataTab}"]`).style.display = 'block';
+}
