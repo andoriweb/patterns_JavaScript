d942ea50fe67097586cabe05b034dc8ec2192205
andoriweb <andoriweb@gmail.com>
2022-02-05 15:08:16 +0200

    ;

--- geolocation/scripts.js
+++ geolocation/scripts.js
@@ -1,28 +1,28 @@
 function getLocation () {
-  navigator.geolocation.getCurrentPosition(showCoordinat, showError)
+  navigator.geolocation.getCurrentPosition(showCoordinat, showError);
 }
 
 function showCoordinat (position) {
-  document.write('Широта :' + position.coords.latitude + '<br>')
-  document.write('Долгота :' + position.coords.longitude + '<br>')
-  document.write('Точность :' + position.coords.accuracy)
+  document.write('Широта :' + position.coords.latitude + '<br>');
+  document.write('Долгота :' + position.coords.longitude + '<br>');
+  document.write('Точность :' + position.coords.accuracy);
 }
 
 function showError (error) {
   switch (error.code) {
     case error.PERMISSION_DENIED:
-      alert ('Пользователь запретил считывать информацию о его местонахождении.')
-      break
+      alert ('Пользователь запретил считывать информацию о его местонахождении.');
+      break;
     case error.POSITION_UNAVAILABLE:
-      alert ('Браузер не может определить местонахождение.')
-      break
+      alert ('Браузер не может определить местонахождение.');
+      break;
     case error.TIMEOUT:
-      alert ('Браузер не успел определить местонахождение.')
-      break
+      alert ('Браузер не успел определить местонахождение.');
+      break;
     case error.UNKNOWN_ERROR:
-      alert ('Произошла неопределённая ошибка.')
-      break
+      alert ('Произошла неопределённая ошибка.');
+      break;
   }
 }
 
-getLocation ()
+getLocation ();
